c637f132f83209771416eeab32aa77dd3d36d0ee
julia <juliarome@live.fr>
2020-11-19 14:21:59 +0100

    ajout texte History

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -6,17 +6,38 @@ function History() {
     <div className="page-wrapper">
       <header>
         <h1>
-          <i className="fas fa-brain"></i> Sample Lesson Timeline
+          <i className="fas fa-brain"></i> History of Antiquity
         </h1>
         <h2>
-          <i className="fas fa-user"></i> Learning Experience
+          <i className="fas fa-user"></i>
+          Antiquity begins in the 4th millennium BC. The date usually taken to
+          indicate the end of antiquity in the European and Mediterranean
+          civilization, and therefore the beginning of the Middle Age, is the
+          fall of the Western Romans, marked by the deposition of Emperor of the
+          West Romulus Augustule in 476. It is the first period of history; it
+          comes after Prehistory and precedes the Middle Ages. It is generally
+          delimited by the invention of writing, about 3,500 BC, and the fall of
+          the Roman Empire, in 476.
         </h2>
       </header>
 
       <main id="chronology">
         <article className="timeline">
           <div className="time-block">
-            <h3>Lesson 1</h3>
+            <h3> - 3500 Invention of writing in Mesopotamia</h3>
+            <p>
+              The earliest forms of writing were drawn symbols engraved on clay
+              tablets
+            </p>
+          </div>
+        </article>
+
+        <article className="timeline">
+          <div className="time-block">
+            <h3>
+              {" "}
+              -3500 The first great civilizations develop in the Middle East
+            </h3>
             <p>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit.
               Molestias repellendus officia iusto voluptatibus saepe aliquam qui
@@ -27,7 +48,7 @@ function History() {
 
         <article className="timeline">
           <div className="time-block">
-            <h3>Lesson 2</h3>
+            <h3> -1200 Invention of the alphabet by the Phoenicians</h3>
             <p>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit.
               Molestias repellendus officia iusto voluptatibus saepe aliquam qui
@@ -38,18 +59,10 @@ function History() {
 
         <article className="timeline">
           <div className="time-block">
-            <h3>Lesson 3</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Molestias repellendus officia iusto voluptatibus saepe aliquam qui
-              adipisci voluptatum voluptate ipsam.
-            </p>
-          </div>
-        </article>
-
-        <article className="timeline">
-          <div className="time-block">
-            <h3>Lesson 4</h3>
+            <h3>
+              -776 Les premiers jeux olympiques antiques se déroulent à Olympie,
+              en Grèce
+            </h3>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
               voluptatem eum nam?
@@ -59,19 +72,16 @@ function History() {
 
         <article className="timeline">
           <div className="time-block">
-            <h3>Lesson 5</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem,
-              esse ad! Nisi iste amet ipsum aspernatur aliquid? Obcaecati
-              tenetur delectus eum explicabo quia repellendus aliquid debitis
-              asperiores? Iste, quod tempore!
-            </p>
+            <h3>
+              -52 Vercingetorix lays his arms at the feet of Julius Caesar
+            </h3>
+            <p>Conquête de la Gaule par les légions romaines</p>
           </div>
         </article>
 
         <article className="timeline">
           <div className="time-block">
-            <h3>Lesson 6</h3>
+            <h3>0 Naissance de Jésus-Christ (-6 ? - 4 ?)</h3>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. At
               voluptatibus incidunt error repellat, quo soluta consectetur
@@ -82,7 +92,10 @@ function History() {
 
         <article className="timeline">
           <div className="time-block">
-            <h3>Lesson 7</h3>
+            <h3>
+              406 Beginning of the barbarian invasions (Alamans, Burgundians,
+              Vandals, Franks ...) in Roman Gaul
+            </h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
             <p>
               Also, Lorem ipsum, dolor sit amet consectetur adipisicing elit.
@@ -93,7 +106,10 @@ function History() {
 
         <article className="timeline">
           <div className="time-block">
-            <h3>Lesson 8</h3>
+            <h3>
+              476 End of the Western Roman Empire End of the Western Roman
+              Empire
+            </h3>
             <p>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit.
               Molestias repellendus officia iusto voluptatibus saepe aliquam qui
