3eb43274aff3f8d755d0446aa26f867b1420e409
Jérémy Cellier <j.cellier28@gmail.com>
2020-11-19 17:00:42 +0100

    Finish globe

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Switch, Route } from 'react-router-dom';
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Quiz from './components/Quiz';
+import Globe from './components/Globe';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Globe />
       <Quiz />
       {/* <Switch>
         <Route exact path="/" component={History} />
---
+++ src/components/Globe.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import ReactGlobe from "react-globe";
+import './Globe.css';
+
+function Globe() {
+  const markers = [
+    {
+      id: "marker1",
+      color: "red",
+      coordinates: [37.971449, 23.724896],
+      value: 175,
+      name: "Temple of athena",
+      img: 'https://media.discordapp.net/attachments/777923556696260618/778992946133336084/The_Parthenon_in_Athens.jpg?width=915&height=643',
+    },
+    {
+      id: "marker2",
+      color: "red",
+      coordinates: [41.867331, 12.493619],
+      value: 175,
+      name: "Colosseum",
+      img: 'https://cdn.discordapp.com/attachments/777923556696260618/779002686636687420/Colosseum-Rome-Italy.png'
+    },
+    {
+      id: "marker3",
+      city: "Singapour",
+      color: "red",
+      coordinates: [43.947071, 4.535937],
+      value: 175,
+      name:"Gard's Bridge",
+      img:"https://cdn.discordapp.com/attachments/777923556696260618/779011185458151424/9k.png",
+    },
+  ];
+  const [cock, setCock] = useState([]);
+  const [cardVisible, setCard] = useState(false);
+
+  function markerTooltipRenderer() {
+    return !cardVisible ? "Click for more information" : "";
+  }
+
+  function onClickMarker(marker) {
+    setCock(marker)
+    setCard(true);
+  }
+
+  function onDefocus(e) {
+    setCard(false);
+    setCock([]);
+  }
+
+  return (
+    <div className="margin">
+      {cardVisible && (
+        <div
+          onClick={onDefocus}
+          role="treeitem"
+          tabIndex="-1"
+          onKeyDown={onDefocus}
+          id="wrapperCard"
+          style={{
+            padding: 12,
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <div className='cardWrapper'>
+            <div className='cardLittleWrapper'>
+                <h2>{cock.name}</h2>
+                <img src={cock.img} alt='description'></img>
+            </div>
+          </div>
+        </div>
+      )}
+        <ReactGlobe
+            height="80.5vh"
+            markers={markers}
+            options={{ markerTooltipRenderer }}
+            width="98.9vw"
+            onClickMarker={onClickMarker}
+            onDefocus={onDefocus}
+        />
+    </div>
+  );
+}
+
+export default Globe;
